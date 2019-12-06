49a135b53161048e545282d98546ff5dd4370276
huangy <597505910@qq.com>
2019-12-06 11:40:56 +0800

    update

--- zx-consumer/src/main/java/zx/cn/consumer/controller/ZxController.java
+++ zx-consumer/src/main/java/zx/cn/consumer/controller/ZxController.java
@@ -1,9 +1,11 @@
 package zx.cn.consumer.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -11,8 +13,16 @@ public class ZxController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "fallback")
     @GetMapping("/getAll")
     public List<String> getAllUser(){
         return  restTemplate.getForObject("http://zx-provider/getMember",List.class);
+    }
+
+    public List<String> fallback() {
+        List<String>list=new ArrayList<>();
+        list.add("熔断了");
+        return list;
     }
 }
